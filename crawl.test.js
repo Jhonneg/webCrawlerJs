6a21cd7cff6056d058fd9042a7d45659471cd530
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-11-17 23:28:33 -0300

    added normalize url function

--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,10 @@
 const { normalizeURL } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeURL", () => {
-  const input = "";
+test("normalizeURL strip protocol", () => {
+  const input = "https://jhonneg.is-a.dev/";
   const actual = normalizeURL(input);
-  const expected = "";
+  const expected = "jhonneg.is-a.dev/";
 
   expect(actual).toEqual(expected);
 });
