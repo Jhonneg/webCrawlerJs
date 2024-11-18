42f8a7fee4b4a5d6ce68d6ea6c14c01bd7b9e5de
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-11-17 23:56:21 -0300

    added normalizeURL functionality

--- crawl.test.js
+++ crawl.test.js
@@ -2,9 +2,30 @@ const { normalizeURL } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
-  const input = "https://jhonneg.is-a.dev/";
+  const input = "https://jhonneg.is-a.dev";
   const actual = normalizeURL(input);
-  const expected = "jhonneg.is-a.dev/";
-
+  const expected = "jhonneg.is-a.dev";
   expect(actual).toEqual(expected);
 });
+
+test("normalizeURL strip last slash", () => {
+  const input = "https://jhonneg.is-a.dev/";
+  const actual = normalizeURL(input);
+  const expected = "jhonneg.is-a.dev";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://JHONNEG.is-a.dev";
+  const actual = normalizeURL(input);
+  const expected = "jhonneg.is-a.dev";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://jhonneg.is-a.dev";
+  const actual = normalizeURL(input);
+  const expected = "jhonneg.is-a.dev";
+  expect(actual).toEqual(expected);
+});
+
